4bd91fd8c8318ca63221d227072e0cf4389670dc
Marek Mościchowski <marek.moscichowski@miquido.com>
2025-10-13 11:58:25 +0200

    chore: Update SNS formatter

--- formatters.tf
+++ formatters.tf
@@ -1,5 +1,5 @@
 module "default_formatter" {
-  source = "git::https://github.com/miquido/terraform-default-sns-formatter.git?ref=tags/1.0.4"
+  source = "git::https://github.com/miquido/terraform-default-sns-formatter.git?ref=tags/1.0.5"
 
   environment = var.environment
   project     = var.project
